9fa4e4f68c86a11e92a8dab49ccd7e3b2a351f88
kmsdevdata-sketch <kmsdevdata@gmail.com>
2025-12-22 16:03:09 +0900

    servlet으로 web page개발

---
+++ src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,23 @@
+package hello.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private Long id;
+    private String userName;
+    private int age;
+
+    public Member() {
+    }
+
+    public Member(String name, int age) {
+        this.userName = name;
+        this.age = age;
+    }
+}
+
+
