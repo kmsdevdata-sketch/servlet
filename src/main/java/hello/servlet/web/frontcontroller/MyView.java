52781a54368e2637c4956c416c98fd72f328e777
kmsdevdata-sketch <kmsdevdata@gmail.com>
2025-12-23 18:15:12 +0900

    ControllerV4

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -15,6 +15,11 @@ public class MyView {
         this.viewPath = viewPath;
     }
 
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request,response);
+    }
+
     public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
         modelToRequestAttribute(model, request);
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
